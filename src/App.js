af300421c129a4e330a22894974728f79a9d4b96
puci2020 <adam.pucicki97@gmail.com>
2020-09-15 23:58:49 +0200

    Add form, material-ui and make function to add items to list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from "./Theme/Layout";
+import {Button} from '@material-ui/core'
+
 
 
 function App() {
+    const [todos, setTodos] = useState(['one', 'two','three']);
+    const [input, setInput] = useState('');
+
+    const addTodo = (event) => {
+        event.preventDefault();
+        setTodos([...todos, input]);
+        setInput('');
+    };
+
     return (
         <Layout>
             <div className="App">
-                siema
+                <h1>To Do App with Firebase</h1>
+                <form>
+                <input value={input} onChange={event => setInput(event.target.value)}/>
+                <Button variant="contained" color="primary" type="submit" onClick={addTodo}>Add Todo</Button>
+                </form>
+                <ul>
+                    {todos.map(todo => (
+                        <li>{todo}</li>
+                    ))}
+                    {/*<li></li>*/}
+                    {/*<li></li>*/}
+                </ul>
             </div>
         </Layout>
     );
--- src/Theme/GlobalStyles.js
+++ src/Theme/GlobalStyles.js
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     overflow-x: hidden;
+    text-align: center;
   }  
 `;
 
